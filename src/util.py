bff3174aa7fb3bba248aa7ad9916f39a812afd45
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-16 16:23:40 +0600

    only one hour and I'm already needing 4 util functions

---
+++ src/util.py
@@ -0,0 +1,20 @@
+# Imports
+from colorama import Fore, Style
+import os
+
+# Functions
+def print_coloured(message):
+    pass
+
+def break_line(amount=1):
+    print("\n" * amount)
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def title(title):
+    if os.name == "nt":
+        os.system(f"title {title}") # Windows only
+
+def wait(to_exit=False):
+    input(f"Press Enter to {'exit' if to_exit else 'continue'}.\n")

2cd2cf391d024e03db1b643c5eae7fdaa700db00
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-17 13:54:34 +0600

    implemented config reader + menu complete except for game making

--- src/engine.py
+++ src/engine.py
@@ -1,6 +1,9 @@
 # Imports
 import questionary
 from util import clear, break_line, wait
+from get_config import Config
+from packs_viewer import PacksViewer
+from packs_detector import get_zipped
 
 # Making the app
 class Engine:
@@ -13,7 +16,10 @@ class Engine:
             "Quit"
         )
         self.selection = -1
-        self.main_menu_loop()
+        self.config_loader = Config("config.toml")
+
+        # Looping
+        self.main_menu_loop()  # ALWAYS GO LAST
 
     def main_menu_loop(self):
         while True:
@@ -32,11 +38,24 @@ class Engine:
 
             # Loading choices
             match self.selection:
+                case 0:
+                    break_line()
+                    packs = [x["name"] for x in get_zipped()]
+                    selected_pack = questionary.select(
+                        "Please select a pack (Ctrl+C to cancel):",
+                        choices=packs
+                    ).ask(kbi_msg="")
+                    if selected_pack is not None:
+                        pass
+
+                case 1:
+                    PacksViewer(self)
+
                 case 2:
                     break_line()
                     print("Made by R1DF. (https://github.com/R1DF/)")
                     print("Coded using Python with questionary and colorama.")
-                    print("\nSpecial mention to Adam.")
+                    print("\nSpecial mention to Adam and Aru.")
                     wait()
 
                 case 3:

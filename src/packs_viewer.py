2ee94ad0bebb06465dc67fb242b94b3b9f20dc16
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-17 13:55:16 +0600

    packs viewer "menu"

---
+++ src/packs_viewer.py
@@ -0,0 +1,32 @@
+# Base import
+import os
+import json
+from menu import BaseMenu
+from time import sleep
+from util import wait
+from packs_detector import get_zipped
+
+# Packs viewer class
+class PacksViewer(BaseMenu):
+    def __init__(self, master):
+        BaseMenu.__init__(self, master)
+
+    def load(self):
+        print("Loading packs...")
+        sleep(self.master.config_loader.constants["RATE"])
+
+        # Fetching all packs in the folder
+        packs_zipped = get_zipped()
+        if len(packs_zipped) == 0:
+            print("You don't have any pack installed.\n")
+            wait(True)
+        else:
+            print(f"\n{len(packs_zipped)} {'pack was' if len(packs_zipped) == 1 else 'packs_were'} detected.")
+            for file_index in range(len(packs_zipped)):
+                pack_metadata = packs_zipped[file_index]
+                pack_name = pack_metadata["name"]
+                pack_author = pack_metadata["author"]
+                pack_description = pack_metadata["description"]
+                print(f"{file_index + 1}. {pack_name} (by {pack_author})\n{pack_description}\n\n")
+            wait(True)
+

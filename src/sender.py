6366ee82e7e643fb81b21fe6ded2c38733df2e14
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-18 13:52:08 +0600

    snakes and ladders are so similar to the point not using inheritance would put me on the same level as a kindergartener

---
+++ src/sender.py
@@ -0,0 +1,14 @@
+class Sender:
+    def __init__(self, master, sender_type, data):
+        # Initialization
+        self.master = master
+        self.sender_type = sender_type
+        self.data = data
+
+        self.transports_to = self.data["coords"][1]
+        self.question = self.data["question"]
+        self.answers = self.data["answers"]
+
+
+    def send(self):
+        pass

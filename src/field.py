608f563818cb67b6d8a7495a21fd7c486634b832
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-19 15:14:09 +0600

    the width is now intentionally impossible to change might remove that in a later update idk

--- src/field.py
+++ src/field.py
@@ -7,7 +7,7 @@ class Field:
         # Attributes
         self.master = master
         self.pack_data = pack_data
-        self.width, self.height = self.pack_data["grid"]
+        self.width, self.height = 10, self.pack_data["grid_height"]
 
         # Getting snakes and ladders (pun not intended) along with their starting coordinates (index: 0)
         self.snakes = self.pack_data["locations"]["snakes"]
@@ -16,7 +16,7 @@ class Field:
         self.ladders_coords = [x["coords"][0] for x in self.ladders]
 
         # Making the grid
-        self.grid = [[Location(self.master, [x, y]) for x in range(self.width)] for y in range(self.height)]  # list comprehensions are incredible
+        self.grid = [[Location(self, [x, y]) for x in range(self.width)] for y in range(self.height)]  # list comprehensions are incredible
 
         # Adding all snakes and ladders
         for y in range(len(self.grid)):
@@ -27,7 +27,20 @@ class Field:
 
         for y in range(len(self.grid)):
             for x in range(len(self.grid[y])):
-                if [x, y] in self.snakes_coords:
+                if [x, y] in self.ladders_coords:
                     ladder_data = self.snakes[self.ladders_coords.index([x, y])]
                     self.grid[y][x].set_contents("LADDER", ladder_data)
 
+    def show_points(self):
+        # Getting formatted points
+        printed_grid = []
+        for y in range(len(self.grid)):
+            row = self.grid[y][::] if y % 2 == 0 else self.grid[y][::-1]
+            printed_grid.append([x.get_display_name() for x in row])
+
+        # Printing out the formatted points
+        for row in printed_grid[::-1]:
+            print(" | ".join(row))
+
+    def show_players(self):
+        pass

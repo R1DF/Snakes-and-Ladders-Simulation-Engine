bd100f5dedf7474efa31f36c28b5ae49f280a863
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-19 15:15:09 +0600

    beginning work on players

---
+++ src/player.py
@@ -0,0 +1,13 @@
+# Imports
+
+# Player class
+class Player:
+    def __init__(self, master, name):
+        # Initialization
+        self.master = master
+        self.name = name
+        self.coordinates = [0, 0]
+
+    def move(self, units):
+        pass
+

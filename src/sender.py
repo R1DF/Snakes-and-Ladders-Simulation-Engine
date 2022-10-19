845744495be4c007173cd714ce19205ad64b8f9c
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-19 15:15:00 +0600

    made Sender have a None value as its display name

--- src/sender.py
+++ src/sender.py
@@ -4,6 +4,7 @@ class Sender:
         self.master = master
         self.sender_type = sender_type
         self.data = data
+        self.display_name = None
 
         self.transports_to = self.data["coords"][1]
         self.question = self.data["question"]

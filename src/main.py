ff0317e3b554d19b230bb9e4fe4bd085c1518add
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-20 12:26:01 +0600

    added keyboardinterrupt handling for both integrity checks and the actual program

--- src/main.py
+++ src/main.py
@@ -16,16 +16,26 @@ clear()
 title("Loading...")
 
 # If passed, continue checking
-if (missing_file := Check().check_file_integrity()) is not None:
-    if missing_file != "packs":
-        alert(f"\nThe file {missing_file} is missing.\nPlease reinstall the game.\n\n")
-    else:
-        alert("\nThe \"packs\" folder inside the game path was not found.\nPlease make it or reinstall the game.\n\n")
-    wait(True)
-    quit()
+try:
+    if (missing_file := Check().check_file_integrity()) is not None:
+        if missing_file != "packs":
+            alert(f"\nThe file {missing_file} is missing.\nPlease reinstall the game.\n\n")
+        else:
+            alert("\nThe \"packs\" folder inside the game path was not found.\nPlease make it or reinstall the game.\n\n")
+        wait(True)
+        quit()
+except KeyboardInterrupt:
+    break_line()
+    notify("Integrity check skipped.\nHINT: You can disable integrity checks with the settings file.")
+    wait()
 
 # Running
 if __name__ == "__main__":
-    title(f"Snakes and Ladders Simuation Engine {VERSION}")
-    app = Engine(VERSION)
-    clear()
+    try:
+        title(f"Snakes and Ladders Simuation Engine {VERSION}")
+        app = Engine(VERSION)
+        clear()
+    except KeyboardInterrupt:
+        clear()
+        notify("Game force quitted.")
+        quit()

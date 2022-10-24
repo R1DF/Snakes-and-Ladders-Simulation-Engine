c174dce4e2bcbe26b8e486f9fc69f09fe3c47eba
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-24 11:42:13 +0600

    HEAVILY improved movement to make it actually work without bugs and added shortener function (also brought back debug cap for a while)

--- src/player.py
+++ src/player.py
@@ -10,21 +10,36 @@ class Player:
         self.coordinates = [0, 0]
         self.redemption_points = 0
 
+    def on_inverse_row(self):
+        return self.master.must_inverse(self.coordinates[1])
+
     def move(self, units):
+        # Getting values and shortening
         grid_width = 10  # Technically a constant but can be replaced in a future update
         grid_height = self.master.pack_data["grid_height"]
+        x, y = self.coordinates
+        new_x = x + units if not self.on_inverse_row() else x - units
 
-        # Checking positioning
-        if self.coordinates[0] + units >= grid_height:  # Determines if the player moves up or not
-            if self.coordinates[1] != grid_height - 1:  # Can't move further if you're at the top
-                cycles = (self.coordinates[0] + units) // 10
-                moves = self.coordinates[0] + units - (cycles * 10)
-                self.coordinates[0] = moves
-                self.coordinates[1] += cycles
+        # Checking whether the player will go up
+        goes_up = x + units >= grid_width if not self.on_inverse_row() else x - units < 0
+        if goes_up and self.coordinates[1] != grid_height - 1:
+            # Cycles - change in Y, Moves - new X but updated depending on row type
+            if not self.on_inverse_row():
+                cycles = abs(new_x) // grid_height
+                moves = abs(new_x - (cycles * grid_height))
+            else:
+                cycles = (abs(new_x) // grid_height) + 1
+                moves = (abs(new_x) % grid_height) - 1
+
+            # Updating
+            self.coordinates[1] += cycles
+            self.coordinates[0] = moves if not self.on_inverse_row() else self.master.inverse_x_coordinate(moves)
+
         else:
-            if self.coordinates[0] + units <= grid_width:
-                self.coordinates[0] += units
+            # VERY HARD mathematics below (nothing like the previous section)
+            self.coordinates[0] = new_x  # very complicated, don't you think?
 
+        # Checking if a special location was reached
         location = self.master.field.grid[self.coordinates[1]][self.coordinates[0]]
 
         # Checking for location type
@@ -49,5 +64,5 @@ class Player:
 
 
     def get_position(self):
-        return f"({self.coordinates[0] + 1}, {self.coordinates[1] + 1})"
+        return f"({self.coordinates[0]}, {self.coordinates[1]})"
 

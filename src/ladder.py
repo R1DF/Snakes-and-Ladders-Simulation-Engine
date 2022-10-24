6717bef64c63b4b4109295915274396af0bc4281
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-24 14:41:17 +0600

    updated ladder so it is coloured now

--- src/ladder.py
+++ src/ladder.py
@@ -5,6 +5,6 @@ from sender import Sender
 class Ladder(Sender):
     def __init__(self, master, data):
         Sender.__init__(self, master, "LADDER", data)
-        self.display_name = "#" * self.master.master.master.string_representative_width
+        self.display_name_raw = "#" * self.master.master.master.string_representative_width
+        self.display_name = self.display_name_raw
         self.destination_display_name = "H" * self.master.master.master.string_representative_width
-

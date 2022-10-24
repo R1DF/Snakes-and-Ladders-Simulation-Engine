4c3ba637f7ffe9053b3283b673de759270eb681b
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-24 14:41:26 +0600

    updated snake so it is coloured now

--- src/snake.py
+++ src/snake.py
@@ -5,6 +5,7 @@ from sender import Sender
 class Snake(Sender):
     def __init__(self, master, data):
         Sender.__init__(self, master, "LADDER", data)
-        self.display_name = "S" * self.master.master.master.string_representative_width
+        self.display_name_raw = "S" * self.master.master.master.string_representative_width
+        self.display_name = self.display_name_raw
         self.destination_display_name = "U" * self.master.master.master.string_representative_width
 

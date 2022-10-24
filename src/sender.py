3592b9b0db0e8c94e4183adfd5c51d0a688aa045
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-24 14:41:47 +0600

    senders have their own set colour function

--- src/sender.py
+++ src/sender.py
@@ -5,10 +5,26 @@ class Sender:
         self.sender_type = sender_type
         self.data = data
         self.display_name = None
-
-        self.transports_to = self.data["coords"][1]
+        self.display_name_raw = None
+        self.destination = self.data["coords"][1]
         self.questions = self.data["questions"]
-
 
     def send(self):
         pass
+
+    def set_colour(self, colour):
+        match colour:
+            case "RED":
+                self.display_name = f"R#{self.display_name_raw}~|"
+            case "BLUE":
+                self.display_name = f"B#{self.display_name_raw}~|"
+            case "YELLOW":
+                self.display_name = f"Y#{self.display_name_raw}~|"
+            case "CYAN":
+                self.display_name = f"C#{self.display_name_raw}~|"
+            case "MAGENTA":
+                self.display_name = f"M#{self.display_name_raw}~|"
+            case "WHITE":
+                self.display_name = f"W#{self.display_name_raw}~|"
+            case "GREEN":
+                self.display_name = f"G#{self.display_name_raw}~|"

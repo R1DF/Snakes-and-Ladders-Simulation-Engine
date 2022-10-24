0add3b4a498d21a7474bb5ff22679dfbbca5c44b
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-24 18:34:00 +0600

    added players shower and made colouring less harsh on the eyes

--- src/field.py
+++ src/field.py
@@ -28,11 +28,13 @@ class Field:
         self.all_colours = [
             "RED",
             "BLUE",
+            "YELLOW",
             "MAGENTA",
             "CYAN",
-            "WHITE",
             "GREEN"
         ]
+        self.snake_and_ladder_colours = self.all_colours.copy()
+        self.snake_and_ladder_colours.remove("YELLOW")  # Yellow is for redemption points
 
         # Making the grid
         self.grid = [[Location(self, [x, y]) for x in range(self.width)] for y in range(self.height)]  # list comprehensions are incredible
@@ -54,13 +56,13 @@ class Field:
 
         # Colouring all snakes and ladders as well
         for snake_index in range(len(self.snake_objects)):
-            colour = self.all_colours[snake_index % (len(self.all_colours) - 1)]
+            colour = self.snake_and_ladder_colours[snake_index % (len(self.snake_and_ladder_colours))]
             snake_object = self.snake_objects[snake_index].contents
             snake_object.set_colour(colour)
             self.grid[snake_object.destination[1]][snake_object.destination[0]].set_colour(colour)
 
         for ladder_index in range(len(self.ladder_objects)):
-            colour = self.all_colours[ladder_index % (len(self.all_colours) - 1)]
+            colour = self.snake_and_ladder_colours[ladder_index % (len(self.snake_and_ladder_colours))]
             ladder_object = self.ladder_objects[ladder_index].contents
             ladder_object.set_colour(colour)
             self.grid[ladder_object.destination[1]][ladder_object.destination[0]].set_colour(colour)
@@ -69,7 +71,7 @@ class Field:
         for redemption_point in self.redemption_point_coordinates:
             self.grid[redemption_point[1]][redemption_point[0]].add_redemption_point()
 
-    def show_points(self):
+    def show_points(self, player):
         # Getting formatted points
         printed_grid = []
         for y in range(len(self.grid)):
@@ -78,3 +80,23 @@ class Field:
         # Printing out the formatted points and printing out the lists in reverse
         for row in printed_grid[::-1]:
             print(" | ".join(row))
+
+    def show_players(self):
+        # Getting formatted map
+        printed_grid = []
+        for y in range(len(self.grid)):
+            printed_grid.append([x.location_string for x in self.grid[y][::]])
+
+        # Adding players
+        player_names = [x.display_name for x in self.master.players]
+        player_coordinates = [x.coordinates for x in self.master.players]
+
+        # Evey place with a player will be coloured
+        for x, y in player_coordinates:
+            printed_grid[y][x] = f"Y#{printed_grid[y][x]}~|"
+
+        # Printing out all players in the grid (ensuring that the grid is reversed)
+        for row in printed_grid[::-1]:
+            print(" | ".join([util.get_coloured_message(x) for x in row]))
+        return list(zip(player_names, player_coordinates))
+

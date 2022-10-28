e0ac1e81aca860189ddbcb5c385d029fba8f6718
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-28 17:46:43 +0600

    implemented pack verifier

--- src/packs_detector.py
+++ src/packs_detector.py
@@ -1,11 +1,12 @@
 # Imports
+from pack_verifier import PackVerifier
 import os
 import json
 
 # Detection function
 def detect():
     file_names = [x for x in os.listdir(os.getcwd() + "\\packs\\") if x.endswith((".json"))]
-    opened_files = [json.load(open(os.getcwd() + "\\packs\\" + x)) for x in file_names]
+    opened_files = [json.load(open(os.getcwd() + "\\packs\\" + x)) for x in file_names if PackVerifier.verify(os.getcwd() + "\\packs\\" + x, "1")[0]]
     pack_names = [x["meta"]["name"] for x in opened_files]
     pack_authors = [x["meta"]["author"] for x in opened_files]
     pack_descriptions = [x["meta"]["description"] for x in opened_files]

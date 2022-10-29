7812c4e9b12edaf867580d05f14a866355a36194
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-29 19:08:22 +0600

    mandates added settings checks (specifically with file integrity checking)

--- src/main.py
+++ src/main.py
@@ -3,9 +3,23 @@ try:
     from util import *
     from integrity_check import Check
     from engine import Engine
+    from get_config import Config
+    should_check_integrity = Config("config.toml").data["program"]["CHECK_FILE_INTEGRITY"]
 
 except ImportError as ie:
-    alert(f"Error importing game files.\nException message: {ie}\nPlease reinstall the game.\n")
+    alert(f"Error importing game files.\nException message: {ie}\nPlease reinstall the game.")
+    wait(True)
+    quit()
+
+except FileNotFoundError as fnfe:
+    alert(f"Error searching for the configuration file.\nException message: {fnfe}\nPlease reinstall the game.")
+    wait(True)
+    quit()
+
+except Exception as e:
+    alert(f"Ran into unexpected exception.\nException message: {e}\nPlease reinstall the game.\nIf the error persists, "
+          f"submit an issue at https://github.com/R1DF/Snakes-and-Ladders-Simulation-Engine about your error.")
+    wait(True)
     quit()
 
 # CONSTANTS
@@ -16,18 +30,19 @@ clear()
 title("Loading...")
 
 # If passed, continue checking
-try:
-    if (missing_file := Check().check_file_integrity()) is not None:
-        if missing_file != "packs":
-            alert(f"\nThe file {missing_file} is missing.\nPlease reinstall the game.\n\n")
-        else:
-            alert("\nThe \"packs\" folder inside the game path was not found.\nPlease make it or reinstall the game.\n\n")
-        wait(True)
-        quit()
-except KeyboardInterrupt:
-    break_line()
-    notify("Integrity check skipped.\nHINT: You can disable integrity checks with the settings file.")
-    wait()
+if should_check_integrity:
+    try:
+        if (missing_file := Check().check_file_integrity()) is not None:
+            if missing_file != "packs":
+                alert(f"\nThe file {missing_file} is missing.\nPlease reinstall the game.\n\n")
+            else:
+                alert("\nThe \"packs\" folder inside the game path was not found.\nPlease make it or reinstall the game.\n\n")
+            wait(True)
+            quit()
+    except KeyboardInterrupt:
+        break_line()
+        notify("Integrity check skipped.\nHINT: You can disable integrity checks in the settings menu.")
+        wait()
 
 # Running
 if __name__ == "__main__":
